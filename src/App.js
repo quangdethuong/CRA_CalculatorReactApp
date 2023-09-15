b8ff763a6d6b00595be9bc50e4397c288860d4e3
quangdethuong <quangnn1106@gmail.com>
2023-09-15 23:39:49 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper';
+import Screen from './components/Screen';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => (
+          <Button
+            key={i}
+            value={btn}
+          />
+        ))}
+      </ButtonBox>
+      <h1>App</h1>
+    </Wrapper>
   );
 }
 
---
+++ src/components/Screen.jsx
@@ -0,0 +1,9 @@
+
+
+const Screen = () => {
+    return (
+        <div className="screen">010101</div>
+    )
+}
+
+export default Screen
